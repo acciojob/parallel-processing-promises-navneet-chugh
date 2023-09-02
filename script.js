118421d85b997f8e14b67525d373e0947b401e42
navneet-chugh <navneetchugh15@gmail.com>
2023-09-02 22:09:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
-const btn = document.getElementById("download-images-button");
+const downloadButton = document.getElementById("download-images-button");
 
 const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
+  { url: "https://picsum.photos/id/237/200/300", alt: "Image 1" },
+  { url: "https://picsum.photos/id/238/200/300", alt: "Image 2" },
+  { url: "https://picsum.photos/id/239/200/300", alt: "Image 3" },
 ];
+
 downloadButton.addEventListener('click', () => {
-            downloadImages(imageUrls)
-                .then(images => {
-                    displayImages(images);
-                })
-                .catch(error => {
-                    console.error(error);
-                });
+    downloadImages(images)
+        .then(images => {
+            displayImages(images);
+        })
+        .catch(error => {
+            console.error(error);
         });
+});
 
-        function downloadImages(imageUrls) {
-            const promises = imageUrls.map(image => {
-                return fetch(image.url)
-                    .then(response => {
-                        if (!response.ok) {
-                            throw new Error(`Failed to load image's URL: ${image.url}`);
-                        }
-                        return response.blob();
-                    })
-                    .then(blob => {
-                        const imageUrl = URL.createObjectURL(blob);
-                        return { url: imageUrl, alt: image.alt };
-                    });
+function downloadImages(images) {
+    const promises = images.map(image => {
+        return fetch(image.url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Failed to load image's URL: ${image.url}`);
+                }
+                return response.blob();
+            })
+            .then(blob => {
+                const imageUrl = URL.createObjectURL(blob);
+                return { url: imageUrl, alt: image.alt };
             });
+    });
 
-            return Promise.all(promises);
-        }
+    return Promise.all(promises);
+}
 
-        function displayImages(images) {
-            outputDiv.innerHTML = '';
-            images.forEach(image => {
-                const imgElement = document.createElement('img');
-                imgElement.src = image.url;
-                imgElement.alt = image.alt;
-                outputDiv.appendChild(imgElement);
-            });
-        }
+function displayImages(images) {
+    output.innerHTML = '';
+    images.forEach(image => {
+        const imgElement = document.createElement('img');
+        imgElement.src = image.url;
+        imgElement.alt = image.alt;
+        output.appendChild(imgElement);
+    });
+}
